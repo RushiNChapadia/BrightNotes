97416afe2725ef9528f0e73fe8aef726efcf51c2
SaiAvinash2002 <saiavinashvenkata@gmail.com>
2026-01-13 11:13:54 -0600

    added all usecases

--- app/src/main/java/com/example/newapp5/domain/usecase/InsertNotesUsecase.kt
+++ app/src/main/java/com/example/newapp5/domain/usecase/InsertNotesUsecase.kt
@@ -1,11 +1,12 @@
 package com.example.newapp5.domain.usecase
 
+import com.example.newapp5.data.NotesEntity
 import com.example.newapp5.domain.repository.NotesRepository
 import javax.inject.Inject
 
 class InsertNotesUsecase @Inject constructor(val notesRepository: NotesRepository){
 
-    suspend fun invoke(){
-
+    suspend fun invoke(notesEntity: NotesEntity){
+        notesRepository.insertNotesToLocal(notesEntity)
     }
 }
